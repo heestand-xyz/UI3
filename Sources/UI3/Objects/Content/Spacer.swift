edb06b2c849af99899afd9746e5dff6169a80483
Hexagons <anton@hexagons.se>
2019-09-28 10:17:36 +0200

    restructure

---
+++ Sources/UI3/Objects/Content/Spacer.swift
@@ -0,0 +1,48 @@
+//
+//  Spacer.swift
+//  UI3
+//
+//  Created by Hexagons on 2019-09-27.
+//
+
+import SceneKit
+
+public struct Spacer: UI3Content {
+    
+    public var width: CGFloat? = nil
+    public var height: CGFloat? = nil
+    public var length: CGFloat? = nil
+    
+    // MARK: - Life Cycle
+    
+    public init() {}
+    
+    // MARK: - Node
+    
+    public func node(frame: UI3Frame) -> SCNNode {
+        
+        return SCNNode()
+        
+    }
+    
+    // MARK: - Frame
+    
+    public func frame(width: CGFloat? = nil, height: CGFloat? = nil, length: CGFloat? = nil) -> UI3Object {
+        return self
+    }
+    
+    // MARK: - Content
+    
+    public func color(_ value: UIColor) -> UI3Content {
+        return self
+    }
+    
+    public func shading(_ value: UI3Shading) -> UI3Content {
+        return self
+    }
+    
+    public func isDoubleSided(_ value: Bool) -> UI3Content {
+        return self
+    }
+    
+}
